8d985bc887e383531829fa3f417a07c9ff18b586
bk62 <bkafle862@gmail.com>
2019-09-11 20:38:07 +0545

    Added player model and animator -- end of tutorial part 3

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+    // takes .1s to transition betn animations
+    const float locomotionAnimSmoothTime = 0.1f;
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+        animator = GetComponentInChildren<Animator> ();
+    }
+
+    void Update () {
+        // current speed / max speed is betn 0..1
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimSmoothTime, Time.deltaTime);
+    }
+}
